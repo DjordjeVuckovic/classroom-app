a0d6a665701f1eeae759e5c76f387f1943541b54
DjordjeVuckovic <vuckovic.ra8.2019@uns.ac.rs>
2023-02-16 23:09:22 +0100

    feat: project created

---
+++ src/test/java/com/studycode/classroombe/ClassroomBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studycode.classroombe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
